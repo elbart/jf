8847d70527843afd5cdc96b56203c97736d9a0c2
Tim Eggert <tim@elbart.com>
2018-04-04 23:49:33 +0200

    remove warnings

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,18 @@
 extern crate encoding_rs;
 extern crate serde_json;
 
-use std::io::{self, BufRead, Read};
+use std::io::Read;
 
 use encoding_rs::UTF_8;
 use serde_json::{Error, Map, Value};
 
-const VERSION: &'static str = env!("CARGO_PKG_VERSION");
-
 fn main() {
     let mut stdin = std::io::stdin();
-    let json = string_from_reader(&mut stdin).expect("Failed to read from stdin");
+    let json = read_json_to_string(&mut stdin).expect("Failed to read from stdin");
 
     let d = match parse_json(&json) {
         Ok(x) => flatten_value(&x, ""),
-        Err(e) => "Invalid JSON".to_string(),
+        Err(_) => "Invalid JSON".to_string(),
     };
 
     println!("{}", d);
@@ -70,11 +68,7 @@ fn flatten_array(arr: &Vec<Value>, path: &str) -> String {
     return s;
 }
 
-/// Safely decode from a Read trait to a String by correctly handling potential
-/// UTF-8 BOMs, etc. To do this, we go via the encoding_rs rather than reading
-/// directly to a Rust string which leaves the BOM in place, causing Serde to
-/// fail when it sees a codepoint it knows isn't valid JSON.
-fn string_from_reader(reader: &mut Read) -> Result<String, std::io::Error> {
+fn read_json_to_string(reader: &mut Read) -> Result<String, std::io::Error> {
     let mut buffer = Vec::new();
     reader.read_to_end(&mut buffer)?;
     let (res, _) = UTF_8.decode_with_bom_removal(&buffer);
