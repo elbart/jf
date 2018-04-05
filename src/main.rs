be063c83dabcea11f04905bd63f4ae469dd89f96
Tim Eggert <tim.eggert@celeraone.com>
2018-04-05 07:27:39 +0200

    use BufWriter instead of temporary strings
    funnily, that made things slower :(

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate encoding_rs;
 extern crate serde_json;
 
-use std::io::Read;
+use std::io::{Read, Write, BufWriter, Stdout};
 
 use encoding_rs::UTF_8;
 use serde_json::{Error, Map, Value};
@@ -10,23 +10,21 @@ fn main() {
     let mut stdin = std::io::stdin();
     let json = read_json_to_string(&mut stdin).expect("Failed to read from stdin");
 
-    let d = match parse_json(&json) {
-        Ok(x) => flatten_value(&x, ""),
-        Err(_) => "Invalid JSON".to_string(),
-    };
+    let mut writer = std::io::BufWriter::new(std::io::stdout());
 
-    println!("{}", d);
+    match parse_json(&json) {
+        Ok(x) => flatten_value(&x, "", &mut writer),
+        Err(_) => (),
+    }
 }
 
-fn flatten_value(data: &Value, path: &str) -> String {
+fn flatten_value(data: &Value, path: &str, writer: &mut BufWriter<Stdout>) {
     if is_scalar(data) {
-        format!("{} = {};\n", path, data.to_string())
+        write!(writer, "{} = {};\n", path, data.to_string());
     } else if data.is_object() {
-        flatten_object(data.as_object().unwrap(), path)
+        flatten_object(data.as_object().unwrap(), path, writer)
     } else if data.is_array() {
-        flatten_array(data.as_array().unwrap(), path)
-    } else {
-        "".to_string()
+        flatten_array(data.as_array().unwrap(), path, writer)
     }
 }
 
@@ -38,34 +36,27 @@ fn is_scalar(value: &Value) -> bool {
     }
 }
 
-fn flatten_object(obj: &Map<String, Value>, path: &str) -> String {
-    let mut s = String::new();
+fn flatten_object(obj: &Map<String, Value>, path: &str, writer: &mut BufWriter<Stdout>) {
     for (k, v) in obj {
         if v.is_object() {
-            s.push_str(format!("{}.{} = {{}};\n", path, k).as_str())
+            write!(writer, "{}.{} = {{}};\n", path, k);
         } else if v.is_array() {
-            s.push_str(format!("{}.{} = [];\n", path, k).as_str())
+            write!(writer, "{}.{} = [];\n", path, k);
         }
 
-        s.push_str(flatten_value(v, format!("{}.{}", path, k).as_str()).as_str());
+        flatten_value(v, format!("{}.{}", path, k).as_str(), writer);
     }
-
-    return s;
 }
 
-fn flatten_array(arr: &Vec<Value>, path: &str) -> String {
-    let mut s = String::new();
-
+fn flatten_array(arr: &Vec<Value>, path: &str, writer: &mut BufWriter<Stdout>) {
     for (idx, v) in arr.iter().enumerate() {
         if v.is_object() {
-            s.push_str(format!("{}[{}] = {{}};\n", path, idx).as_str())
+            write!(writer, "{}[{}] = {{}};\n", path, idx);
         } else if v.is_array() {
-            s.push_str(format!("{}[{}] = [];\n", path, idx).as_str())
+            write!(writer, "{}[{}] = [];\n", path, idx);
         }
-        s.push_str(flatten_value(v, format!("{}[{}]", path, idx).as_str()).as_str());
+        flatten_value(v, format!("{}[{}]", path, idx).as_str(), writer);
     }
-
-    return s;
 }
 
 fn read_json_to_string(reader: &mut Read) -> Result<String, std::io::Error> {
